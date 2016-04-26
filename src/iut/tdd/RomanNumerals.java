7c8e397482cef96647cd6999f09daba2215b1c43
Adel Tihianine <tihiania@epicea26.iut-infobio.priv.univ-lille1.fr>
2016-04-26 16:33:09 +0200

    add_classes

---
+++ src/iut/tdd/RomanNumerals.java
@@ -0,0 +1,12 @@
+package iut.tdd;
+
+public class RomanNumerals {
+	public String convertToRoman(String arabe) {
+		return "I";
+	}
+
+	public String convertFromRoman(String roman) {
+		return "1";
+	}
+
+}
---
+++ src/iut/tdd/RomanNumeralsTest.java
@@ -0,0 +1,20 @@
+package iut.tdd;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RomanNumeralsTest {
+
+	@Test
+	public void should_return_I_when_1 () {
+		//Given
+		String input = "1";
+		String expected = "I";
+		RomanNumerals romanNumeral = new RomanNumerals();
+		//When
+		String actual = romanNumeral.convertToRoman(input);
+		//Then
+		Assert.assertEquals(expected, actual);
+	}
+	
+}
